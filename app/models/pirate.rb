6bebb8bebf8751c676cb67c3fec022e43ab802b6
“aroncey” <artuhrroncey@gmail.comssh-keygen -t rsa -C arthurroncey@gmail.com>
2017-02-21 14:50:38 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+  attr_reader :name, :weight, :height
+
+  PIRATES = []
+
+  def initialize(hash)
+    @name = hash[:name]
+    @weight = hash[:weight]
+    @height = hash[:height]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
 end
